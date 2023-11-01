ca5a51425df602d4222b79da55706828017c2196
Aarushi <aarushi0015.be21@chitkara.edu.in>
2023-11-01 19:04:11 +0530

    Weather app using react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState} from "react";
+import axios from 'axios';
+import WeatherApp from "./Components/WeatherApp/WeatherApp";
 function App() {
+
+  const cityName = 'Delhi'; // Replace with 'Delhi' or any other desired city name
+  const apiKey = '2e51c8f7f4c66066441fea2eb9ab1ca1';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+  
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <WeatherApp/> 
     </div>
   );
 }
