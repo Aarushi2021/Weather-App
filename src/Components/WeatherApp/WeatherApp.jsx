ec8ebe2bd937715312fb0c8a95b4662907eee818
Aarushi <101499776+Aarushi2021@users.noreply.github.com>
2023-11-01 19:11:50 +0530

    Add files via upload
    This is the corrected jsx file or weather app

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -34,8 +34,8 @@ const WeatherApp = () => {
     const location = document.getElementsByClassName("weather-location");
     
       humidity[0].innerHTML = data.main.humidity+" %";
-      wind[0].innerHTML = Math.floor(data.wind.speed+" km/h");
-      temperature[0].innerHTML = Math.floor(data.main.temp+" °c");
+      wind[0].innerHTML = Math.floor(data.wind.speed)+" km/h";
+      temperature[0].innerHTML = Math.floor(data.main.temp)+" °c";
       location[0].innerHTML = data.name;
    
     if(data.weather[0].icon==="01d" || data.weather[0].icon==="01n"){
